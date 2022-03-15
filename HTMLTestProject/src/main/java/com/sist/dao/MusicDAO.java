5a2213e16463259fab49f333783d91311e4d7705
future <83049262+future-mi@users.noreply.github.com>
2022-03-15 20:14:06 +0900

    HTML

---
+++ HTMLTestProject/src/main/java/com/sist/dao/MusicDAO.java
@@ -0,0 +1,54 @@
+package com.sist.dao;
+import java.util.*;
+import java.sql.*;
+public class MusicDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private final String URL="jdbc:oracle:thin:@localhost:1521:XE";
+	
+	public MusicDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception ex) {}
+	}
+	// 2. 오라클 연결
+	public void getConnection() {			
+		try {
+			conn=DriverManager.getConnection(URL,"hr","happy");
+		} catch (Exception ex) {}
+	}
+	// 3. 오라클 해제
+	public void disConnection() {
+		try {
+			if(ps!=null) ps.close(); 
+			if(conn!=null) conn.close(); 
+		} catch (Exception ex) {}
+	}
+	// 데이터를 묶어서 → 브라우저로 전송
+	public List<MusicVO> musicListData() {
+		List<MusicVO> list=new ArrayList<MusicVO>();
+		try {
+			getConnection();
+			String sql="SELECT no,title,singer,poster "
+					  +"FROM genie_music "
+					  +"ORDER BY no ASC";
+			ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				MusicVO vo=new MusicVO();
+				vo.setNo(rs.getInt(1));
+				vo.setTitle(rs.getString(2));
+				vo.setSinger(rs.getString(3));
+				vo.setPoster(rs.getString(4));
+				
+				list.add(vo);
+			}
+			rs.close();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			disConnection();
+		}
+		return list;
+	}
+}
